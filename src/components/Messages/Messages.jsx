7cd30e25e57683ae53210abdff2d1dd8161240be
Kristina <145868344+slkristina@users.noreply.github.com>
2025-09-02 15:01:29 +0300

    message-component (#15)
    * Added saving of contact form messages in Firebase
    
    * Added saving of contact form messages in Firebase

---
+++ src/components/Messages/Messages.jsx
@@ -0,0 +1,28 @@
+import {addDoc, collection} from "firebase/firestore";
+import {db} from "../../firebase/FirebaseConfig.js";
+import {useEffect} from "react";
+
+
+function Messages({name, email, message}) {
+    async function sendMessageToFirebase() {
+        try {
+            await addDoc(collection(db, "messages"), {
+                name,
+                email,
+                message,
+                createdAt: new Date(),
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    window.alert("Jouw bericht is succesvol verzonden");
+
+    useEffect(() => {
+            sendMessageToFirebase();
+        },
+        []);
+}
+
+export default Messages;
