4021688fd902539dfcddf0741c8c54d5bc2737fd
slkristina <christinagaudi@gmail.com>
2025-10-19 22:22:09 +0200

    - get rid of duplicated code for fetching adventures and use unmounting in useeffect

---
+++ src/util/fetchers.jsx
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export default async function fetchAdventures() {
+    try {
+        const response = await axios.get(
+            "https://firestore.googleapis.com/v1/projects/scribbels-b3ffe/databases/(default)/documents/adventures"
+        );
+        return response.data.documents?.map(doc => ({
+           id: doc.name.split("/").pop(),
+           title: doc.fields?.title?.stringValue || "",
+           youtubeUrl: doc.fields?.youtube_url?.stringValue || null,
+           spotifyUrl: doc.fields?.spotify_url?.stringValue || null,
+           thumbnailUrl: doc.fields?.thumbnail_url?.stringValue || null,
+           omgeving: doc.fields?.omgeving?.arrayValue?.values?.map(v => v.stringValue) || [],
+           seizoen: doc.fields?.seizoen?.arrayValue?.values?.map(v => v.stringValue) || [],
+       })) || [];
+    } catch (err) {
+        console.error("Error fetching messages", err.message);
+    }
+}
