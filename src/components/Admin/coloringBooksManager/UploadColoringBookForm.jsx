502aabd012c7d6f1cdfcb12ece23b1e0a174a380
slkristina <christinagaudi@gmail.com>
2025-10-19 22:20:19 +0200

    - upload coloringbooks works now(!)

---
+++ src/components/Admin/coloringBooksManager/UploadColoringBookForm.jsx
@@ -0,0 +1,89 @@
+import React, {useState} from "react";
+import {db} from "../../../firebase/firebaseConfig.js";
+import {addDoc, collection} from "firebase/firestore";
+
+function UploadColoringBookForm() {
+    const [title, setTitle] = useState("");
+    const [image, setImage] = useState("");
+
+    function handleFileUpload(event) {
+        const file = event.target.files[0];
+        if (!file) return;
+
+        if (file.type !== "image/svg+xml") {
+            alert("Alleen .svg bestanden toegestaan");
+            return;
+        }
+        const reader = new FileReader();
+        reader.onload = (e) => setImage(e.target.result)
+        reader.readAsText(file);
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        // if (!title || !image) {
+        //     alert("Vul alle velden in!");
+        //     return;
+        // }
+
+        try {
+            const data = {
+                title: title,
+                image: image,
+                createdAt: new Date()
+            };
+
+            await addDoc(collection(db, "coloringBooks"), data);
+            alert("Uw verhaal is opgeslagen");
+
+            handleReset();
+        } catch (error) {
+            alert("Fout bij het opslaan!");
+            console.error(error);
+        }
+    }
+
+    function handleReset() {
+        setTitle("");
+        setImage("");
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="title">
+                Titel (verplicht)
+                <input
+                    id="title"
+                    type="text"
+                    name="title"
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                    required
+                />
+            </label>
+
+            <label htmlFor="image">
+                Kleurplaat in .svg formaat (verplicht)
+                <input
+                    id="image"
+                    type="file"
+                    name="image"
+                    accept={".svg"}
+                    onChange={handleFileUpload}
+                    required
+                />
+            </label>
+            <button type="submit">
+                Creëren!
+            </button>
+            <button type="button"
+                    onClick={handleReset}>
+                Reset
+            </button>
+        </form>
+    );
+}
+
+export default UploadColoringBookForm;
+
